fe9e72ff0ac9b6db79377f1e44fad767ba8b9db5
ramafraxa <121234912+ramafraxa@users.noreply.github.com>
2024-11-16 18:33:08 +0300

    Update mafra.py

--- mafra.py
+++ mafra.py
@@ -8,24 +8,30 @@ from selenium.webdriver.common.by import By
 app = Flask(__name__)
 
 def download_selenium():
-    # Configurer les options Chrome
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    try:
+        # Configurer les options Chrome
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
 
-    # Installer et configurer le service pour ChromeDriver
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    
-    # Charger la page Google
-    driver.get("https://www.google.com/")
-    
-    # Récupérer le titre de la page
-    title = driver.title
-    driver.quit()
+        # Installer et configurer le service pour ChromeDriver
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        
+        # Charger la page Google
+        driver.get("https://www.google.com/")
+        
+        # Récupérer le titre de la page
+        title = driver.title
+        driver.quit()
 
-    # Retourner le titre dans un dictionnaire
-    return jsonify({'Page title': title})
+        # Retourner le titre dans un dictionnaire
+        return jsonify({'Page title': title})
+
+    except Exception as e:
+        # Capturer et retourner l'erreur
+        return jsonify({'error': str(e)}), 500
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
