c0ca74eeeb0e864c0f8cad50727954f5b80b1db6
ramafraxa <121234912+ramafraxa@users.noreply.github.com>
2024-11-17 09:10:51 +0300

    Update mafra.py

--- mafra.py
+++ mafra.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 
 app = Flask(__name__)
 
@@ -11,18 +8,16 @@ def download_selenium():
     try:
         # Configurer les options Chrome
         chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')  # Exécution sans interface graphique
+        chrome_options.add_argument('--no-sandbox')  # Nécessaire pour les environnements Docker
         chrome_options.add_argument('--disable-dev-shm-usage')
 
-        # Installer et configurer le service pour ChromeDriver
-        driver = webdriver.Chrome( service=Service(ChromeDriverManager(version="130.0.6723.116").install()),options=chrome_options)
+        # Créer une instance de WebDriver
+        driver = webdriver.Chrome(options=chrome_options)
 
-
-        
         # Charger la page Google
         driver.get("https://www.google.com/")
-        
+
         # Récupérer le titre de la page
         title = driver.title
         driver.quit()
